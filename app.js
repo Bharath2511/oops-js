f75929eaa8cfbbad013b147be913211f4e4572ec
V S Bharath <bharathcse5t6@gmail.com>
2018-10-08 23:30:57 +0530

    method-chaining

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ class User {
     constructor(email,name) {
          this.email = email
          this.name = name
+         this.score = 0
     }
     login() {
         console.log(this.email, 'just logged in')
+        return this
     }
     logout() {
         console.log(this.email, 'just logged out')
+        return this
+    }
+    updateScore() {
+        this.score++
+        console.log(this.email, 'score is now', this.score )
+        return this
     }
 }
 
 var userOne = new User('doe.edu','Joe')
 console.log(userOne)
-userOne.login()
-userOne.logout()
+//method chaining
+userOne.login().updateScore().updateScore().logout()
 // the new keyword
 // creates an empty object
 //sets the value of "this" to be the new empty object
