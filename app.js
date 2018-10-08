94035f86130826dd4799c09424739c22ae5c32c1
V S Bharath <bharathcse5t6@gmail.com>
2018-10-08 23:18:27 +0530

    adding functions to the classes

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ class User {
          this.email = email
          this.name = name
     }
+    login() {
+        console.log(this.email, 'just logged in')
+    }
+    logout() {
+        console.log(this.email, 'just logged out')
+    }
 }
 
 var userOne = new User('doe.edu','Joe')
 console.log(userOne)
+userOne.login()
+userOne.logout()
 // the new keyword
 // creates an empty object
 //sets the value of "this" to be the new empty object
