48c86108d265df6c6ef081c2030984341cd437ea
V S Bharath <bharathcse5t6@gmail.com>
2018-10-08 22:06:52 +0530

    objects and methods

--- app.js
+++ app.js
@@ -2,5 +2,13 @@
 const userOne = {
     email : 'john@gmail.com',
     name : 'John',
+    login() {
+        console.log(this.email,'has logged in')
+    },
+    logout() {
+        console.log(this.email, 'has logged out')
+    }
 }
+userOne.login()
+userOne.logout()
 console.log(userOne.name)
