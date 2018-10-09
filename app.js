229e655f25343d3ff60bc685a69adf908b8089fd
V S Bharath <bharathcse5t6@gmail.com>
2018-10-09 12:50:33 +0530

    prototype inheritance part 2

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ function User(email,name) {
    this.online = false
 }
 
-User.prototype.login = () => {
+User.prototype.login = function() {
     this.online =true
     console.log(this.email, 'has logged in')
+}
+
+User.prototype.logout = function(){
+    this.online = false
+    console.log(this.email, 'has logged out')
 }
 var userOne = new User('doe.edu','Joe')
 var userTwo = new User('snow.edu','John')
 
 console.log(userOne)
 userTwo.login()
+userOne.logout()
